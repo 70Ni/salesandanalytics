e99788d1c3262f8e65a79853d58e3581b40431a5
70ni <4thedevelopments@gmail.com>
2024-08-05 13:21:57 +0530

    Table (created)

---
+++ src/Components/Table/Table.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import Paid from "../Others/Paid";
+
+import Recent from "../../api/Recent.json";
+console.log(Object.keys(Recent[0]));
+let NamingHeades = Object.keys(Recent[0]).splice(1, 1);
+
+function Table() {
+  return (
+    <table className="text-left">
+      <tr className="Header wrapper  border-b-2 border-general-40">
+        <td className="text-text2reg px-2 py-3 text-general-80">Name</td>
+        <td className="text-text2reg px-2 py-3 text-general-80">Date</td>
+        <td className="text-text2reg px-2 py-3 text-general-80">Amount</td>
+        <td className="text-text2reg px-2 py-3 text-general-80">Status</td>
+      </tr>
+      {Recent.map((x) => (
+        <tr className="content-wrapper border-b-[1px] border-general-40">
+          <td className="text-text2med px-2 py-4 text-general-100">{x.Name}</td>
+          <td className="text-text2reg px-2 py-4 text-general-100">{x.Date}</td>
+          <td className="text-text2reg px-2 py-4 text-general-100">
+            {x.Amount}
+          </td>
+          <Paid status={x.status} />
+        </tr>
+      ))}
+      {/* <tr className="content-wrapper border-b-[1px] border-general-40">
+        <td className="text-text2med px-2 py-4 text-general-100">Jessica S.</td>
+        <td className="text-text2reg px-2 py-4 text-general-100">24.05.2020</td>
+        <td className="text-text2reg px-2 py-4 text-general-100">24.05</td>
+        <Paid status={"Paid"} />
+      </tr> */}
+    </table>
+  );
+}
+
+export default Table;
