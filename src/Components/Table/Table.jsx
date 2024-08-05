d7ebf226903430ec5674a6efa362084c418d4142
70ni <4thedevelopments@gmail.com>
2024-08-05 19:17:26 +0530

    Table Example (created)

--- src/Components/Table/Table.jsx
+++ src/Components/Table/Table.jsx
@@ -3,34 +3,41 @@ import Paid from "../Others/Paid";
 
 import Recent from "../../api/Recent.json";
 console.log(Object.keys(Recent[0]));
-let NamingHeades = Object.keys(Recent[0]).splice(1, 1);
+let NamingHeades = Object.keys(Recent[0]);
 
-function Table() {
+function Table({ Header }) {
   return (
-    <table className="text-left">
-      <tr className="Header wrapper  border-b-2 border-general-40">
-        <td className="text-text2reg px-2 py-3 text-general-80">Name</td>
-        <td className="text-text2reg px-2 py-3 text-general-80">Date</td>
-        <td className="text-text2reg px-2 py-3 text-general-80">Amount</td>
-        <td className="text-text2reg px-2 py-3 text-general-80">Status</td>
-      </tr>
-      {Recent.map((x) => (
-        <tr className="content-wrapper border-b-[1px] border-general-40">
-          <td className="text-text2med px-2 py-4 text-general-100">{x.Name}</td>
-          <td className="text-text2reg px-2 py-4 text-general-100">{x.Date}</td>
-          <td className="text-text2reg px-2 py-4 text-general-100">
-            {x.Amount}
-          </td>
-          <Paid status={x.status} />
+    <div className="px-7 pt-7 pb-5 border rounded-[6px] w-full border-general-40">
+      <div className="Header text-paraBold text-left mb-5 text-general-100 ">
+        Recent Transactions
+      </div>
+      <table className="text-left  border-zinc-400 w-full">
+        <tr className="Header wrapper  border-b-2 border-general-40">
+          {Object.keys(Recent[0])
+            .splice(1, Recent.length)
+            .map((x) => (
+              <th className="text-text2reg px-2 py-3 text-general-80">{x}</th>
+            ))}
+          {/* <th className="text-text2reg px-2 py-3 text-general-80">Date</th>
+        <th className="text-text2reg px-2 py-3 text-general-80">Amount</th>
+        <th className="text-text2reg px-2 py-3 text-general-80">Status</th> */}
         </tr>
-      ))}
-      {/* <tr className="content-wrapper border-b-[1px] border-general-40">
-        <td className="text-text2med px-2 py-4 text-general-100">Jessica S.</td>
-        <td className="text-text2reg px-2 py-4 text-general-100">24.05.2020</td>
-        <td className="text-text2reg px-2 py-4 text-general-100">24.05</td>
-        <Paid status={"Paid"} />
-      </tr> */}
-    </table>
+        {Recent.map((x) => (
+          <tr className="content-wrapper border-b-[1px] border-general-40 last:border-b-0">
+            <td className="text-text2med px-2 py-4 text-general-100">
+              {x.Name}
+            </td>
+            <td className="text-text2reg px-2 py-4 text-general-100">
+              {x.Date}
+            </td>
+            <td className="text-text2reg px-2 py-4 text-general-100">
+              {x.Amount}
+            </td>
+            <Paid status={x.status} />
+          </tr>
+        ))}
+      </table>
+    </div>
   );
 }
 
