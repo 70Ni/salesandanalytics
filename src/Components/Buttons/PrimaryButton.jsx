385b11b718ae4457eed0e19db1ffc5064ba096e1
70ni <4thedevelopments@gmail.com>
2024-09-02 20:44:51 +0530

    Button Handle Click changes

--- src/Components/Buttons/PrimaryButton.jsx
+++ src/Components/Buttons/PrimaryButton.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 let Icons =
   "container flex w-full h-fit items-center px-5 py-3 rounded bg-primary-100 justify-center cursor-pointer";
@@ -6,8 +7,23 @@ let text =
   "container  w-full h-fit flex items-center px-6 py-3 rounded bg-primary-100 justify-center cursor-pointer";
 
 function PrimaryButton(props) {
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    // if (props.action.type === "Navigation") {
+    //   navigate(props.action.link); // Navigate to the About Page
+    // } else if (props.action) {
+    //   console.log("Not defined");
+    // }
+    if (!props.action) {
+      console.log("Not defined");
+    } else if (props.action.type === "Navigation") {
+      navigate(props.action.link);
+    }
+  };
+
   return (
-    <div className={props.icon ? Icons : text}>
+    <div className={props.icon ? Icons : text} onClick={handleClick}>
       {props.icon ? props.icon : null}
       {props.icon && <img src={props.icon} alt="" className="mr-1" />}
       <div className=" text-white text-para text whitespace-nowrap">
