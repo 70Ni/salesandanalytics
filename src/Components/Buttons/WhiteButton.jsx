a22cd95006ae5e7c8fcfca99ba493086d4e11d16
70ni <4thedevelopments@gmail.com>
2024-09-04 11:59:59 +0530

    Navigation button change

--- src/Components/Buttons/WhiteButton.jsx
+++ src/Components/Buttons/WhiteButton.jsx
@@ -1,16 +1,28 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 let Icons =
-  "container flex w-full items-center px-5 py-3 rounded justify-center border-[1px] border-general-50 cursor-pointer";
+  "container flex w-full h-fit  items-center px-5 py-3 rounded justify-center border-[1px] border-general-50 cursor-pointer bg-white";
 let text =
-  "container  w-full flex items-center px-6 py-3 rounded justify-center border-[1px] border-general-50 cursor-pointer";
+  "container  w-full h-fit flex items-center px-6 py-3 rounded justify-center border-[1px] border-general-50 cursor-pointer bg-white";
 
 function WhiteButton(props) {
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    if (!props.action) {
+      console.log("Not defined");
+    } else if (props.action.type === "Navigation") {
+      navigate(props.action.link);
+    }
+  };
   return (
-    <div className={props.icon ? Icons : text}>
-      <img src={props.icon} alt="" className="mr-[10px]" />
+    <div className={props.icon ? Icons : text} onClick={handleClick}>
+      {props.icon && <img src={props.icon} alt="" className="mr-[10px]" />}
       {props.icon ? props.icon : null}
-      <div className=" text-primary-100 text-para text ml-1">{props.text}</div>
+      <div className=" text-primary-100 text-para text whitespace-nowrap">
+        {props.text}
+      </div>
     </div>
   );
 }
