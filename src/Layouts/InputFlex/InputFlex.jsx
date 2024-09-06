b5e852212bf366a5d8a45e8f65142328f1427bd3
70ni <4thedevelopments@gmail.com>
2024-09-06 11:04:09 +0530

    Input Flex section

---
+++ src/Layouts/InputFlex/InputFlex.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import Input from "../../Components/Input/Medium/Input";
+
+function InputFlex({ title, placeholder }) {
+  return (
+    <div className="flexbt gap-6 max-[484px]:flex-wrap">
+      <Input
+        title={title[0]}
+        placeholder={placeholder ? placeholder[0] : null}
+      />
+      <Input
+        title={title[1]}
+        placeholder={placeholder ? placeholder[0] : null}
+      />
+    </div>
+  );
+}
+
+export default InputFlex;
