89f1842ae7aa52c03adcef24601935ed268a910e
70ni <4thedevelopments@gmail.com>
2024-09-27 15:23:33 +0530

    button changes

--- src/Components/Buttons/WhiteButton.jsx
+++ src/Components/Buttons/WhiteButton.jsx
@@ -2,9 +2,9 @@ import React from "react";
 import { useNavigate } from "react-router-dom";
 
 let Icons =
-  "container flex w-full h-fit  items-center px-5 py-3 rounded justify-center border-[1px] border-general-50 cursor-pointer bg-white";
+  "container flex w-full h-fit  items-center p-p20 p-p12 rounded justify-center border-[1px] border-general-50 cursor-pointer bg-white";
 let text =
-  "container  w-full h-fit flex items-center px-6 py-3 rounded justify-center border-[1px] border-general-50 cursor-pointer bg-white";
+  "container  w-full h-fit flex items-center px-p24 p-p12 rounded justify-center border-[1px] border-general-50 cursor-pointer bg-white";
 
 function WhiteButton(props) {
   const navigate = useNavigate();
