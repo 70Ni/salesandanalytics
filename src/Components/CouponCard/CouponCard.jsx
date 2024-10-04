daf117e54f9d85b2c2d3157c2f245697a450ca1b
70ni <4thedevelopments@gmail.com>
2024-10-04 14:37:59 +0530

    Coupon Card padding changed to common variable

--- src/Components/CouponCard/CouponCard.jsx
+++ src/Components/CouponCard/CouponCard.jsx
@@ -3,7 +3,7 @@ import icons from "../../Images/Icons/02 Common/05 Heart.svg";
 function CouponCard({ content, icon }) {
   return (
     <>
-      <div className="max-w-[244px] w-full border border-[1px] rounded border-general-50 py-6 flex flex-col  items-center cursor-pointer hover:border-primary-70">
+      <div className="max-w-[244px] w-full border border-[1px] rounded border-general-50 py-p24 flex flex-col  items-center cursor-pointer hover:border-primary-70">
         <img src={icons} alt="" />
         <div className="text-para text-primary-100 mt-3">Fixed Discount</div>
       </div>
